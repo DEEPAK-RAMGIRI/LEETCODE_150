8ce5a8e742a3817921cf83e8ee851a1d91f4b87f
RAMGIRI  DEEPAK KUMAR <142658497+DEEPAK-RAMGIRI@users.noreply.github.com>
2025-12-18 10:20:24 +0530

    Create 01.Merge Sorted Array.py

---
+++ 01.Merge Sorted Array.py
@@ -0,0 +1,9 @@
+# 88. Merge Sorted Array
+# Time Complexcity O(n log n)
+# Space Complexcity O(1)
+for i in range(n):
+            nums1[m+i] = nums2[i]
+        nums1.sort()
+
+# Time Complexcity O(m + n)
+# Space Complexcity O(1)
